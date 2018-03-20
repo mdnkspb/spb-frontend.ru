dc611b2e4f10c136052979cb2225ab4b4bad2cc5
Alex Kurganov <me@akurganow.ru>
2018-03-20 14:17:52 +0000

    Updates [ci skip]

---
+++ path---person-andrey-gurylev-2e9a5ec1857e238585f6.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x6a77b87b8aff4400],{"./node_modules/json-loader/index.js!./.cache/json/person-andrey-gurylev.json":function(e,n){e.exports={pathContext:{data:{id:"c3CyuHKiJWok2SM6IWskqw2",name:"Андрей",lastname:"Гурылев",position:null,vk:null,telegram:null,twitter:null,slack:"forgrew",photo:{file:{url:"//images.ctfassets.net/ej3wzulnf8dw/2XVzUcE6gwIEsUKuoEAq64/f287f16ae69d9f64dbedc068d7032e5c/WbhCQmxnEKQ.jpg"}},company:null,podcasts:[{title:"#26 — «Грустный»",number:26}]},id:19}}}});
+//# sourceMappingURL=path---person-andrey-gurylev-2e9a5ec1857e238585f6.js.map
---
+++ path---person-vsevolod-rodionov-40b7a3d7689fa3e185af.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xb46891400fec0000],{"./node_modules/json-loader/index.js!./.cache/json/person-vsevolod-rodionov.json":function(e,n){e.exports={pathContext:{data:{id:"c6nSLqX7e2AsQoCyiqsY8gk",name:"Всеволод",lastname:"Родионов",position:null,vk:null,telegram:null,twitter:null,slack:"jabher",photo:{file:{url:"//images.ctfassets.net/ej3wzulnf8dw/3s7HoykDvGg8qkMeKg4AM4/dd7598e63eeba3aa06c17df1ad7296fb/rUe0sAyBNl8.jpg"}},company:null,podcasts:[{title:"#8 — «Что такое FrontOps»",number:8}]},id:24}}}});
+//# sourceMappingURL=path---person-vsevolod-rodionov-40b7a3d7689fa3e185af.js.map
