31d1b652f0de3e72aaf3aaca04f0298bf253f343
Alex Kurganov <me@akurganow.ru>
2018-03-22 18:31:41 +0000

    Updates [ci skip]

---
+++ path---person-tim-marinin-1a317728217217f88554.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x56f6249b1985f800],{"./node_modules/json-loader/index.js!./.cache/json/person-tim-marinin.json":function(n,e){n.exports={pathContext:{data:{id:"c28bm76VLkkYcO0UEo6SaU6",name:"Тим",lastname:"Маринин",position:null,vk:null,telegram:null,twitter:null,slack:null,photo:{file:{url:"//images.ctfassets.net/ej3wzulnf8dw/7tTvT643nyQsE2aUkCAyiw/9a894ce2dc9ed76adcbcb657a76f5f58/8vzDi3kk778.jpg"}},company:null,podcasts:[{title:"#6 — «Тим Маринин и мышки на тёрке»",number:6}]},id:27}}}});
+//# sourceMappingURL=path---person-tim-marinin-1a317728217217f88554.js.map
