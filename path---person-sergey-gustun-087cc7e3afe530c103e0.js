7b74c2da0859f7eaeaac054af9e38e8c424be99c
Alex Kurganov <me@akurganow.ru>
2018-04-01 14:51:38 +0000

    Updates [ci skip]

--- path---person-sergey-gustun-087cc7e3afe530c103e0.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xa0c584634cb7],{444:function(t,e){t.exports={pathContext:{data:{id:"c20xOt1Ju288SSgUEgoA4wK",name:"Сергей",lastname:"Густун",position:null,vk:null,telegram:null,twitter:"https://twitter.com/gaserd123",slack:"gaserd",photo:{file:{url:"//images.ctfassets.net/ej3wzulnf8dw/5jBl3cSONyOOWewYmwSwms/bfd97bffe28457b35a29bc4fd49d85f1/dQH8lNbC6uo.jpg"}},company:"Aviasales, Handsales",podcasts:[{title:"#22 — «Как стать независимым разработчиком»",number:22}],personId:"sergey-gustun"},id:29}}}});
-//# sourceMappingURL=path---person-sergey-gustun-087cc7e3afe530c103e0.js.map
