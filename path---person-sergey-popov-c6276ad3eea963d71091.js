85bac86e6e16a003046585a523cb010a01bc9189
Alex Kurganov <me@akurganow.ru>
2018-04-01 14:46:08 +0000

    Updates [ci skip]

--- path---person-sergey-popov-c6276ad3eea963d71091.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([88059568089493],{446:function(e,t){e.exports={pathContext:{data:{id:"c6qtizJ7KcoKeEEaACAC886",name:"Сергей",lastname:"Попов",position:null,vk:null,telegram:null,twitter:"https://twitter.com/sergeytovarov",slack:"sergeypopov",photo:{file:{url:"//images.ctfassets.net/ej3wzulnf8dw/6BEn6tzk3eIqSsu28cycmC/3be619429b8b02e6b38125e798822e8f/tpKm8k7__400x400.jpg"}},company:null,podcasts:[{title:"#14 — «Ментор успеха залог твоего»",number:14}],personId:"sergey-popov"},id:22}}}});
-//# sourceMappingURL=path---person-sergey-popov-c6276ad3eea963d71091.js.map
