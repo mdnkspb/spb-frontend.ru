1cd9b073ae8052b9c9cac493a05f6a43c942909f
Alex Kurganov <me@akurganow.ru>
2018-04-01 14:52:45 +0000

    Updates [ci skip]

---
+++ path---person-alena-batickaya-0e18a463a6a2bdabf3bd.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xf5377ec1e0f4],{425:function(t,a){t.exports={pathContext:{data:{id:"c2I6UK8U7RCMw2Uqgo6osWU",name:"Алена",lastname:"Батицкая",position:null,vk:null,telegram:null,twitter:"https://twitter.com/ABatickaya",slack:"alenabat",photo:{file:{url:"//images.ctfassets.net/ej3wzulnf8dw/3afKsSJu9aiysSUYC4QEu2/8ff73030feb88e50cc831804e11ecf55/aRWtVguXRzE.jpg"}},company:null,podcasts:[{title:"#15 — «Юные Бобби»",number:15}],personId:"alena-batickaya"},id:10}}}});
+//# sourceMappingURL=path---person-alena-batickaya-0e18a463a6a2bdabf3bd.js.map
