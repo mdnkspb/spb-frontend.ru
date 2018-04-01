138d7f6821ce5a3090eb62cc724d8aa2005c2ea9
Alex Kurganov <me@akurganow.ru>
2018-04-01 18:33:51 +0000

    Updates [ci skip]

---
+++ path---person-aleksey-plutalov-7097fdc9a784c8ab8446.js
@@ -0,0 +1,2 @@
+webpackJsonp([79481404209011],{423:function(e,t){e.exports={pathContext:{data:{id:"c5ULEnropHy8y8oKoQAgWaO",name:"Алексей",lastname:"Плуталов",position:null,vk:null,telegram:null,twitter:"demiazz",slack:"demiazz",photo:{file:{url:"//images.ctfassets.net/ej3wzulnf8dw/3gqF4w43VSq6A8AmQA2Wuk/32aa7764cfb0ddddb14329cb9895c2e5/SMzD5ZQdlFU.jpg"}},company:null,podcasts:[{title:"#8 — «Что такое FrontOps»",number:8}],personId:"aleksey-plutalov"},id:17}}}});
+//# sourceMappingURL=path---person-aleksey-plutalov-7097fdc9a784c8ab8446.js.map
---
+++ path---person-dmitriy-nazarov-8a3423ade5443ebf52fe.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xcf1fa3236f33],{433:function(t,a){t.exports={pathContext:{data:{id:"c4Vt1UebyQgSm6c2qySmYMQ",name:"Дмитрий",lastname:"Назаров",position:null,vk:null,telegram:null,twitter:"nazarov_tech",slack:null,photo:{file:{url:"//images.ctfassets.net/ej3wzulnf8dw/2LRA5RXBwAsuEWOoae4gKc/74dff93854cd085985eff0f9f2894453/pqhamhh8_400x400.jpg"}},company:null,podcasts:[{title:"#18 — «Ребята, давайте жить дружно»",number:18}],personId:"dmitriy-nazarov"},id:9}}}});
+//# sourceMappingURL=path---person-dmitriy-nazarov-8a3423ade5443ebf52fe.js.map
