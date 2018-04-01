94da6e21ad03448f142d524e02c56533c3960040
Alex Kurganov <me@akurganow.ru>
2018-04-01 14:51:10 +0000

    Updates [ci skip]

--- path---person-aleksey-plutalov-e3c2b134da0520506afc.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([79481404209011],{423:function(t,e){t.exports={pathContext:{data:{id:"c5ULEnropHy8y8oKoQAgWaO",name:"Алексей",lastname:"Плуталов",position:null,vk:null,telegram:null,twitter:"https://twitter.com/demiazz",slack:"demiazz",photo:{file:{url:"//images.ctfassets.net/ej3wzulnf8dw/3gqF4w43VSq6A8AmQA2Wuk/32aa7764cfb0ddddb14329cb9895c2e5/SMzD5ZQdlFU.jpg"}},company:null,podcasts:[{title:"#8 — «Что такое FrontOps»",number:8}],personId:"aleksey-plutalov"},id:9}}}});
-//# sourceMappingURL=path---person-aleksey-plutalov-e3c2b134da0520506afc.js.map
