cc212df101565b1eaa36de7461141fc3b56b1fea
Alex Kurganov <me@akurganow.ru>
2018-05-10 18:23:12 +0000

    Updates [ci skip]

---
+++ path---persons-andrey-zaharchenko-d43a6badcdfcea9266f1.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xdccd3b719bf5],{440:function(e,a){e.exports={pathContext:{data:{id:"c1rLXrfso7KyCiWMcmwski",slug:"andrey-zaharchenko",name:"Андрей",lastname:"Захарченко",position:null,vk:null,telegram:null,twitter:null,slack:null,photo:{file:{url:"//images.ctfassets.net/ej3wzulnf8dw/1FFeIfIRhSOqgIKmeeSiuu/c05a34ae1ff8df98e3990a6012f89eb1/____________________.jpg"}},company:"HTML Academy",podcasts:[{title:"#27 — «Тимлидерский»",number:27}]},id:1}}}});
+//# sourceMappingURL=path---persons-andrey-zaharchenko-d43a6badcdfcea9266f1.js.map
